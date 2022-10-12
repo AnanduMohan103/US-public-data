0139181b554a50bb5e8ab04ebf81f17234318953
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-12 08:23:38 +0530

    data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UspubdataComponent } from './uspubdata/uspubdata.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UspubdataComponent
   ],
   imports: [
     BrowserModule,
